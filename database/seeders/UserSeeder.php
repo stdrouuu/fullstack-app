d3ec9621d15832aba37f92f09be91845c66e881e
stdrouuu <brajs0111@gmail.com>
2025-09-24 10:14:23 +0700

    make factory for table users & add seeder for table users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,19 +1,19 @@
 <?php
 
-// php artisan make:seeder ItemSeeder
+// php artisan make:seeder UserSeeder
 
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Item;
+use App\Models\User;
 
-class ItemSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     //Modify
     public function run(): void
     {
-        Item::factory(10)->create(); //panggil model dan factory item dan generate 10 data
+        User::factory(10)->create(); //panggil model dan factory user dan generate 10 data
     }
     //----------------
 }
