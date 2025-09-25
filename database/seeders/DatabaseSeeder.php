ab51ecb47e0744f3108e1ea38ab22efc601a14a6
stdrouuu <brajs0111@gmail.com>
2025-09-25 18:57:56 +0700

    database seeder (generate dummy data with seeder)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            RoleSeeder::class, //seeder role
+            CategorySeeder::class, //seeder category
+            ItemSeeder::class, //factory item
+            UserSeeder::class, //factory user
         ]);
     }
+    // php artisan db:seed => generate random data from seeder
 }
